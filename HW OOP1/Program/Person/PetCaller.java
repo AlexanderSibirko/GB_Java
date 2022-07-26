63f09fd2525ad05840db8b024ba791348f5595e5
AlexanderSibirko <89727680+AlexanderSibirko@users.noreply.github.com>
2022-07-26 15:42:07 +0300

    removed excess method modificator

--- HW OOP1/Program/Person/PetCaller.java
+++ HW OOP1/Program/Person/PetCaller.java
@@ -3,5 +3,5 @@ package Program.Person;
 import Program.Animals.Pets;
 
 public interface PetCaller {
-    public void callPet(Pets[] pets, String phrase);
+    void callPet(Pets[] pets, String phrase);
 }
