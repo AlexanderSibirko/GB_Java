d205e3101704593bc01f0fc28423ac3abcfc0244
AlexanderSibirko <89727680+AlexanderSibirko@users.noreply.github.com>
2022-07-26 16:02:50 +0300

    removed excess method modifiers

--- HW OOP1/Program/Item/ItemManager.java
+++ HW OOP1/Program/Item/ItemManager.java
@@ -1,6 +1,6 @@
 package Program.Item;
 
 public interface ItemManager {
-    public boolean addItem(Item item);
-    public boolean removeItem(Item item);
+    boolean addItem(Item item);
+    boolean removeItem(Item item);
 }
