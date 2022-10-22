68a3ac4219bfde8df9c06be9ab17277fb5270d6b
AlexanderSibirko <AlexanderSibirko@yandex.ru>
2022-10-22 23:53:28 +0300

    forgot to remove comment after testing

--- JavaArchitectHW3/Program.java
+++ JavaArchitectHW3/Program.java
@@ -1,11 +1,7 @@
 package JavaArchitectHW3;
 
-import JavaArchitectHW3.Figures.Circle;
-import JavaArchitectHW3.Figures.Rectangle;
 import JavaArchitectHW3.Figures.ShapeMaker;
 import JavaArchitectHW3.Figures.ShapesArray;
-import JavaArchitectHW3.Figures.Square;
-import JavaArchitectHW3.Figures.Triangle;
 
 public class Program {
     public static void main(String[] args) {
@@ -18,32 +14,32 @@ public class Program {
             System.out.println(e.getMessage());
         }
 
-        // try {
-        //     shapesArray.add(new Rectangle(5,2));
-        //     shapesArray.add(new Rectangle(-5,2));
-        // } catch (Exception e) {
-        //     System.out.println(e.getMessage());
-        // }
+        try {
+            shapesArray.add(ShapeMaker.createRectangle(5,2));
+            shapesArray.add(ShapeMaker.createRectangle(-5,2));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-        // try {
-        //     shapesArray.add(new Rectangle(5,-2));
-        // } catch (Exception e) {
-        //     System.out.println(e.getMessage());
-        // }
+        try {
+            shapesArray.add(ShapeMaker.createRectangle(5,-2));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-        // try {
-        //     shapesArray.add(new Square(5));
-        //     shapesArray.add(new Square(-5));
-        // } catch (Exception e) {
-        //     System.out.println(e.getMessage());
-        // }
+        try {
+            shapesArray.add(ShapeMaker.createSquare(5));
+            shapesArray.add(ShapeMaker.createSquare(-5));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-        // try {
-        //     shapesArray.add(new Triangle(5,3,4));
-        //     shapesArray.add(new Triangle(5,3,9));
-        // } catch (Exception e) {
-        //     System.out.println(e.getMessage());
-        // }
+        try {
+            shapesArray.add(ShapeMaker.createTriangle(5,3,4));
+            shapesArray.add(ShapeMaker.createTriangle(5,3,9));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
         System.out.println("--------------------\nПолучим подсчёт периметров всех фигур");
         System.out.print(shapesArray.calcAllPerimetrs());
