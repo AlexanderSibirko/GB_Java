f6531c2e204e10d040ff8844b1c025f7731c2b81
AlexanderSibirko <AlexanderSibirko@yandex.ru>
2022-11-05 21:51:32 +0300

    refactored

--- JavaArchitectHW3/Figures/Makers/ShapeMaker.java
+++ JavaArchitectHW3/Figures/Makers/ShapeMaker.java
@@ -4,7 +4,6 @@ import JavaArchitectHW3.Figures.Circle;
 import JavaArchitectHW3.Figures.Rectangle;
 import JavaArchitectHW3.Figures.Square;
 import JavaArchitectHW3.Figures.Triangle;
-import JavaArchitectHW3.Figures.Exceptions.IllegalFigureParametrs;
 
 /** simple Factory */
 public class ShapeMaker {
@@ -16,20 +15,20 @@ public class ShapeMaker {
         return instance == null ? new ShapeMaker() : instance;
     }
 
-    public Square createSquare(double side_length) throws IllegalFigureParametrs {
+    public Square createSquare(double side_length) throws IllegalArgumentException {
         return new Square(side_length);
     }
 
-    public Rectangle createRectangle(double side_one_length, double side_two_length) throws IllegalFigureParametrs {
+    public Rectangle createRectangle(double side_one_length, double side_two_length) throws IllegalArgumentException {
         return new Rectangle(side_one_length, side_two_length);
     }
 
-    public Circle createCircle(double radius) throws IllegalFigureParametrs {
+    public Circle createCircle(double radius) throws IllegalArgumentException {
         return new Circle(radius);
     }
 
     public Triangle createTriangle(double side_one_length, double side_two_length, double side_three_length)
-            throws IllegalFigureParametrs {
+            throws IllegalArgumentException {
         return new Triangle(side_one_length, side_two_length, side_three_length);
     }
 }
